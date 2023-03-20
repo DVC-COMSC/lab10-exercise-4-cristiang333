5e575ee96756b1a6737b7907599780dc363c28dd
Cristian Garcia <126625058+cristiang333@users.noreply.github.com>
2023-03-20 06:25:58 +0000

    lab 12

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 def main():
     ##################################################
     # Comlete your code here
-    #################################################
-    celcius = int(input('Enter the Celcius Degree: '))
-    fahrenheit = (9.0 / 5.0) * celcius + 32
-    print(f'The Fahrenheit is {fahrenheit:.2f}')
-  
- #   pass
+   
 
+    import random 
 
+    n1 = random.randint(0,300)
+    n2 = random.randint(0,300)
+    n3 = random.randint(0,300)
+    print (n1, n2, n3)
+
+    if n1 < n2 and n1 < n3:
+      print (f'{n1} is the smallest number')
+    elif n2 < n1 and n2 < n3:
+      print (f'{n2} is the smallest number')
+    else:
+       print (f'{n3} is the smallest number')
 if __name__ == '__main__':
     main()
+
+
+    
